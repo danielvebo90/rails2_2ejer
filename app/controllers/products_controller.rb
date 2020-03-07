83b01bc825ad0cf6cf00a8b6561f6e5efc77f38b
danielvebo <danielvebo@gmail.com>
2020-03-06 20:08:44 -0500

    2 repo

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,45 +1,26 @@
 class ProductsController < ApplicationController
-  protect_from_forgery with: :null_session
-
   def index
-    @products = Product.all
-    render json: @products
-  end
-
-
-  def create
-     @product = Product.create(product_params)
-     if @product.save
-       render json: { product: @product }, status: :created
-     else
-       render json: { errors: @product.errors }, status: 422
-     end
+     @products = Product.all
    end
 
-  def edit
-    @product = Product.find(params[:id])
-  end
+   def new
+     @product = Product.new
+   end
 
-  def update
-    @product = Product.find(params[:id])
-    if @product.update(product_params)
-      render json: @product, status: 200
-    else
-      render json: { errors: @product.errors }, status: 422
-    end
-  end
+   def create
+     @product = Product.create(product_params)
+   end
 
-  def destroy
-    @product = Product.find(params[:id])
-    if @product.destroy
-      render json: @product, status: 204
-    end
+   def edit
+     @product = Product.find(params[:id])
+   end
 
-    head :no_content
-  end
+   def update
+     @product = Product.update(params[:id], product_params)
+   end
 
-  private
-    def product_params
-      params.require(:product).permit(:name, :price)
-    end
+   private
+     def product_params
+       params.require(:product).permit(:name, :price)
+     end
 end
